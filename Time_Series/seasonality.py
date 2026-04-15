2b4f6ecbbb0a5fe8c563562150a0535664c5800d
Craig Murray <cmurray4492@gmail.com>
2026-04-14 22:45:56 -0500

    04 14 2026

---
+++ Time_Series/seasonality.py
@@ -0,0 +1,19 @@
+import random
+import matplotlib.pyplot as plt
+from math import sin
+
+
+if __name__ == '__main__':
+    random.seed(10)
+    length = 100
+    A = 50
+    B = -.05
+    C = 1
+    S =3 
+    trend = [A + B * i for i in range(length)]
+    seasons = [S * sin(i/5) for i in range(length)]
+    noise = [C * random.gauss(0, 1) for _ in range(length)]
+    ts = [trend[i] + noise[i] + seasons[i] for i in range(length)]
+    plt.plot(ts)
+    plt.plot(trend)
+    plt.show()
